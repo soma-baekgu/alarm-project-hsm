f0c1b706618d91e903fbf49957307b60ff0a33cd
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-05-06 14:54:06 +0900

    프로젝트 생성

---
+++ src/test/kotlin/education/alarmproject/AlarmProjectApplicationTests.kt
@@ -0,0 +1,12 @@
+package education.alarmproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class
+AlarmProjectApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
